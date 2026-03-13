eecaaa815b9591c5444e1f36ba3066c23c95b3df
Evoki <timpalcrypto@gmail.com>
2026-03-13 16:14:55 -0700

    Fix reconnect: retry server connection every 5 seconds always

--- timpal.py
+++ timpal.py
@@ -433,7 +433,7 @@ class Network:
                             threading.Thread(target=self._sync_ledger, daemon=True).start()
             except Exception:
                 pass
-            time.sleep(5 if not self.peers else 30)
+            time.sleep(5)
 
     def _bootstrap_connect(self):
         """Connect to bootstrap server and get initial peer list."""
